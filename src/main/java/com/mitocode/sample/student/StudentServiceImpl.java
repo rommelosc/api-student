e303e9a9aef47d056a04964fa9ad22ed5e095943
Oscar Romero <chxt07@gmail.com>
2020-06-26 20:55:25 +0000

    Merged in feature/error-formato-email (pull request #4)
    Error de formato email
    
    * Error formato email 1
    
    * Error formato email 2
    
    * Error formato email 3

--- src/main/java/com/mitocode/sample/student/StudentServiceImpl.java
+++ src/main/java/com/mitocode/sample/student/StudentServiceImpl.java
@@ -44,6 +44,10 @@ public class StudentServiceImpl implements StudentService {
         System.out.println("Cambio ws");
         System.out.println("Cambio ws");
 
+        System.out.println("Error formato email");
+        System.out.println("Error formato email");
+        System.out.println("Error formato email");
+
         if (studentDataAccessService.isEmailTaken(student.getEmail())) {
             throw new ApiRequestException(student.getEmail() + " is taken");
         }
